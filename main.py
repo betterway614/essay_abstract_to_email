5cdb0e08669747d4f5b95d41052f8b25a16630a9
betterway <2214278197@qq.com>
2025-12-19 12:15:10 +0800

    Fix entrypoint sys.path for GitHub Actions

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 import runpy
+import sys
 
 
 def _main():
     root_dir = os.path.dirname(os.path.abspath(__file__))
+    src_dir = os.path.join(root_dir, "src")
+    if src_dir not in sys.path:
+        sys.path.insert(0, src_dir)
     runpy.run_path(os.path.join(root_dir, "src", "main.py"), run_name="__main__")
 
 
